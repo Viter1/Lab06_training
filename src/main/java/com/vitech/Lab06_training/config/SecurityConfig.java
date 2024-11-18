34c3f8f86a75ef628e334b300d7ff832f71b6576
Viter1Qualentum <david.buenano36@comunidadunir.net>
2024-11-18 16:27:41 +0100

    comiteo :D

--- src/main/java/com/vitech/Lab06_training/config/SecurityConfig.java
+++ src/main/java/com/vitech/Lab06_training/config/SecurityConfig.java
@@ -3,33 +3,33 @@ package com.vitech.Lab06_training.config;
 import com.vitech.Lab06_training.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
-    @Value("${token.secret.key}");
     private final String jwtSecret;
 
 
-
-    public SecurityConfig(UserDetailsService userDetailsService, String jwtSecret) {
+    public SecurityConfig(UserDetailsService userDetailsService, @Value("${token.secret.key}") String jwtSecret) {
         this.userDetailsService = userDetailsService;
         this.jwtSecret = jwtSecret;
+
     }
 
 
